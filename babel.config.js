19505969de49eae2a0c91c28e6cd26e20b54f71d
Jeff Lam <lmh.jeff@gmail.com>
2023-05-18 02:04:40 +0800

    commit again

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          allowUndefined: true,
+          safe: true,
+        },
+      ],
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+    ],
+  };
+};
